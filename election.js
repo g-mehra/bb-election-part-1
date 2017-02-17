ff412f98d58d01f27ffd4a536220803020009757
g-mehra <gmehra@alumni.uwo.ca>
2017-02-17 14:28:05 -0500

    git commit -mp

--- election.js
+++ election.js
@@ -1,5 +1,40 @@
 $(document).ready(function() {
 
-  // Imagination!
+
+  $.ajax({
+    url: 'https://bb-election-api.herokuapp.com/',
+    method: 'GET',
+    dataType: 'json'
+
+  }).done(function(responseData){
+
+    responseData.candidates.forEach(function(candidate){
+           var name = candidate.name;
+           var votes = candidate.votes;
+           
+           var liTag = $('<li> Canadidate Name: <br>' + name
+                       + "  <br><br> Vote Count: <br> "
+                       + votes + '<br> </li> <br> <br>')
+
+           $('#can-data').append(liTag);
+          console.log(candidate.name)
+          console.log(candidate.votes)
+    });
+
+
+
+// Jquery way to do above code
+//       $.each(responseData.candidates, function(i, candidate) {
+//       var name = candidate.name;
+//       var votes = candidate.votes;
+//       var liTag = $('<li>' + name + "" + votes)
+//       $('#can-data').append(liTag);
+// debugger
+//       console.log(responseData.name);
+//       console.log(responseData.votes);
+
+    // });
+
+  });
 
 });
